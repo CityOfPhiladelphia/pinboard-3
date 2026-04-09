0a2db8ac27aa1c2787419d1ad61cafa5e626ca56
Theron Halsey <theron.halsey@phila.gov>
2026-04-09 12:13:22 -0400

    push before checkout

--- packages/ui/src/components/PinboardShell.vue
+++ packages/ui/src/components/PinboardShell.vue
@@ -47,7 +47,7 @@ const MobileNavContent: FunctionalComponent = () =>
   display: flex;
   flex-direction: column;
   height: 100vh;
-  width: min(100vw, 1450px); /** Trying out a max width for the app area */
+  /* width: min(100vw, 1450px); */
   margin: auto;
 }
 
