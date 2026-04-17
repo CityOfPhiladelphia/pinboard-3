20a941d5c58e6f24f23172342b46769d8b97f8a3
Theron Halsey <theron.halsey@phila.gov>
2026-04-17 12:14:02 -0400

    commented out code in development

--- packages/ui/src/composables/searchAddress.ts
+++ packages/ui/src/composables/searchAddress.ts
@@ -1,26 +1,26 @@
-import { ref } from 'vue'
-import type { AisAutocompleteResult } from '../types'
+// import { ref } from 'vue'
+// import type { AisAutocompleteResult } from '../types'
 
-const searchString = ref<string>('')
-const searchSuggestions = ref<string[]>([])
+// const searchString = ref<string>('')
+// const searchSuggestions = ref<string[]>([])
 
-async function updateSearchSuggestions(): Promise<string[]> {
-  if (
-    searchString.value.length < 3 ||
-    !/^\d{1,5}(?: [NnSs])? [A-Za-z ]*/.test(searchString.value)
-  ) {
-    return []
-  }
-  const suggestions: AisAutocompleteResult = await (
-    await fetch(
-      `https://ais-autocomplete.citygeo.phila.city/autocomplete?q=${searchString.value.replace(/ /, '+')}`
-    )
-  ).json()
-  const suggestedAddresses = suggestions.count
-    ? Array.from(
-        suggestions.results.addresses,
-        (suggestion) => suggestion.address
-      )
-    : []
-  return suggestedAddresses
-}
+// async function updateSearchSuggestions(): Promise<string[]> {
+//   if (
+//     searchString.value.length < 3 ||
+//     !/^\d{1,5}(?: [NnSs])? [A-Za-z ]*/.test(searchString.value)
+//   ) {
+//     return []
+//   }
+//   const suggestions: AisAutocompleteResult = await (
+//     await fetch(
+//       `https://ais-autocomplete.citygeo.phila.city/autocomplete?q=${searchString.value.replace(/ /, '+')}`
+//     )
+//   ).json()
+//   const suggestedAddresses = suggestions.count
+//     ? Array.from(
+//         suggestions.results.addresses,
+//         (suggestion) => suggestion.address
+//       )
+//     : []
+//   return suggestedAddresses
+// }
