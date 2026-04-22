cfa17932d6d3da000862b3c2ad1ca975a447f78a
Theron Halsey <theron.halsey@phila.gov>
2026-04-22 13:26:58 -0400

    fixed type

--- packages/ui/src/composables/useAddressSearch.ts
+++ packages/ui/src/composables/useAddressSearch.ts
@@ -1,4 +1,4 @@
-import { Ref, ref, toValue, watchEffect } from 'vue'
+import { type Ref, ref, toValue, watchEffect } from 'vue'
 import type { AisAddressSearchResponse, LatLon } from '../types'
 
 export function useAddressSearch(address: string | Ref<string>) {
