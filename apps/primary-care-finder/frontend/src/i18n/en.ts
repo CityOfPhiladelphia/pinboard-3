81eae959583a9c42ffd42f8e16eedac195b48056
Theron Halsey <theron.halsey@phila.gov>
2026-06-29 14:58:47 -0400

    removed unused key

--- apps/primary-care-finder/frontend/src/i18n/en.ts
+++ apps/primary-care-finder/frontend/src/i18n/en.ts
@@ -30,13 +30,12 @@ export default {
     linkText: 'Learn more about getting care',
   },
   app: {
-    title: 'Primary Care Finder',
+    name: 'Primary care finder',
     subtitle: 'Free and low-cost medical care in Philadelphia',
     noResults:
       'No health centers found. Please check your spelling or remove some of your filters, and try again.',
     viewList: 'View list',
     viewMap: 'View map',
-    name: 'Primary care finder',
     aboutTitle: 'About this tool',
     aboutBody:
       'This tool helps Philadelphia residents find free and low-cost primary care providers near them. Search by location, filter by services, and view details like hours, transit options, and available tests.',
