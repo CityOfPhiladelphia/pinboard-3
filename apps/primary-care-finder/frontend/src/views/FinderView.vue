9331370aa805c496bc2d8bce4c2d7c23d9ef9171
Theron Halsey <theron.halsey@phila.gov>
2026-07-27 12:52:35 -0400

    more css fixes

--- apps/primary-care-finder/frontend/src/views/FinderView.vue
+++ apps/primary-care-finder/frontend/src/views/FinderView.vue
@@ -315,10 +315,6 @@ function getMapCardProps(location: PrimaryCareLocation): MapCardProps {
   padding: 1.5rem 1rem 0 1rem;
 }
 
-.abdsf {
-  padding: 1.5rem 1rem 0 1rem;
-}
-
 .locations-callout :deep(.callout-title) {
   font-family: var(--Body-Default-font-body-default-family);
   font-size: var(--scale-200);
