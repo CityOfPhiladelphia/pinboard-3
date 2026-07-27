2530ec5fbcc6ba733ec75e23766720780d3e4862
Theron Halsey <theron.halsey@phila.gov>
2026-07-27 11:59:52 -0400

    css error checking with lint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,15 +1,20 @@
 import js from '@eslint/js'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import tseslint from 'typescript-eslint'
 import eslintPluginVue from 'eslint-plugin-vue'
+import eslintPluginVueScopedCSS from 'eslint-plugin-vue-scoped-css';
 import globals from 'globals'
 import turboPlugin from 'eslint-plugin-turbo'
 
-export const viteJsConfig = [
+export const viteJsConfig = defineConfig([
   globalIgnores(['node_modules/**', 'dist/**', 'cdk.out']),
   js.configs.recommended,
   ...tseslint.configs.strict,
+  turboPlugin.configs['flat/recommended'],
+  ...eslintPluginVue.configs['flat/recommended'],
+  ...eslintPluginVueScopedCSS.configs['flat/recommended'],
+  eslintConfigPrettier,
   {
     files: ['**/*.{js,mjs,ts,vue}'],
     languageOptions: {
@@ -28,9 +33,7 @@ export const viteJsConfig = [
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       'no-undef': 'off',
+      "vue-scoped-css/no-unused-selector": "error"
     },
   },
-  turboPlugin.configs['flat/recommended'],
-  ...eslintPluginVue.configs['flat/recommended'],
-  eslintConfigPrettier,
-]
+])
