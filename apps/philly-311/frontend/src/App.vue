b3d4ed40d3f428b0f38f11f428ee70ebeb127080
Theron Halsey <theron.halsey@phila.gov>
2026-08-27 13:58:40 -0400

    test

--- apps/philly-311/frontend/src/App.vue
+++ apps/philly-311/frontend/src/App.vue
@@ -111,7 +111,7 @@ function login() {
 }
 
 .spinner {
-  width: 3rem;
+  width: 2rem;
   height: 2rem;
   border: 3px solid var(--Schemes-Border-low, #e3e3e3);
   border-top-color: var(--Schemes-Primary, #002855);
